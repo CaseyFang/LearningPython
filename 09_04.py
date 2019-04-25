0431af9fb3be7098c5b7fa459130bc5d71e6aca6
CaseyFang <38835721+CaseyFang@users.noreply.github.com>
2019-04-25 19:10:35 +0800

    Add files via upload

---
+++ 09_04.py
@@ -0,0 +1,24 @@
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+lst = list()
+words = dict()
+
+for line in handle:
+    if not line.startswith("From"):
+        continue
+    else:
+        x=line.split()
+        lst.append(x[1])
+
+for sender in lst:
+    words[sender]=words.get(sender,0)+1
+
+bigcount = None
+bigword = None
+for word,count in list(words.items()):
+    if bigcount is None or count>bigcount:
+        bigword=word
+        bigcount=count
+
+print(bigword,bigcount)
