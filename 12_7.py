9b3dfc6360834f9ac3240448c86c0cdca696370b
CaseyFang <casey_fang@outlook.com>
2019-05-08 22:16:03 +0800

    done

--- 12_7.py
+++ 12_7.py
@@ -14,11 +14,11 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-#http://py4e-data.dr-chuck.net/known_by_Fikret.html
+#http://py4e-data.dr-chuck.net/known_by_Meshach.html
 
 url = input('Enter URL: ')
 if len(url)<1:
-    url = ' http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+    url = ' http://py4e-data.dr-chuck.net/known_by_Meshach.html'
     
 count=float(input("Enter count: "))
 
@@ -26,23 +26,15 @@ print("Retrieving:",url)
 
 html = urllib.request.urlopen(url,context=ctx).read()
 
-if count > 0 :
+while count > 0 :
     soup = BeautifulSoup(html,'html.parser')
     urllst =[]
     tags = soup('a')
     for tag in tags:
         urllst.append(tag.get('href',None))
-    print("Retrieving:",urllst[6])
+    print("Retrieving:",urllst[17])
     count = count-1
-    html = urllib.request.urlopen(urllst[6],context=ctx).read()
+    html = urllib.request.urlopen(urllst[17],context=ctx).read()
 
 
 
-
-#soup = BeautifulSoup(html,'html.parser')
-#tags = soup('a')
-#urllst=[]
-#for tag in tags:
-#create html list
-#    urllst.append(tag.get('href',None))
-#print(urllst[2])
