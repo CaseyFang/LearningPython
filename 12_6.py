78f8586cbd5b2ecfb8402f4e871592fc7810b21f
CaseyFang <casey_fang@outlook.com>
2019-05-08 09:03:36 +0800

    Accessing Web Data Assignment

---
+++ 12_6.py
@@ -0,0 +1,28 @@
+import urllib.request, urllib.parse, urllib.error, urllib.response
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#http://py4e-data.dr-chuck.net/comments_42.html
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url,context=ctx).read()
+soup = BeautifulSoup(html,'html.parser')
+
+count = 0
+value = 0
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    x=float(tag.contents[0])
+    count = count+1
+    value = value +x
+
+print("Count ",int(count))
+print("Sum ",int(value))
+
+
